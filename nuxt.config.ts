10c11f7260a3afb0ea1200892d001748d5fcd45c
divyansh0x0 <divyansh0x0@gmail.com>
2026-01-03 23:57:27 +0530

    Changed config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
+  nitro:{
+    preset: "github_pages",
+  },
+  app: {
+    baseURL: '/'
+  },
   devtools: { enabled: true },
   css:[
-    "assets/css/main.css",
-    "assets/css/colors.css",
+    "/assets/css/main.css",
+    "/assets/css/colors.css",
   ],
   modules: [
     '@nuxt/hints',
-    '@nuxt/image',
     '@nuxt/test-utils',
     '@nuxt/ui',
     '@nuxt/scripts',
